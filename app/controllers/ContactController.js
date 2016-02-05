5e31bb1dbc2409e3d3a4af84f46605a8923d1a2e
Kris Byrum <krisjbyrum@gmail.com>
2016-02-05 13:11:24 -0800

    Worked on PHP Mailer.

--- app/controllers/ContactController.js
+++ app/controllers/ContactController.js
@@ -3,27 +3,11 @@
 
 angular.module("myPortfolio")
 	.controller("ContactController", ["$scope", "$http", "$window", function($scope, $http, $window) {
+		$scope.submission = false;
 		$scope.submitted = function() {
-			var formData = {
-				"firstName": "Kris",
-				"lastName": "Tested",
-				"email": "Email here",
-				"phoneNumber": "phone number",
-				"message": "$scope.message didn't work"
-			}
 			// check if honey pot if empty
 			// var honeyPot = $scope.winnie_the_pooh;
 			// if(honeyPot.value == '') {
-				// $http.post('app/templates/contact.php', formData)
-				// 	.then(function(response) {
-				// 		console.log("successful " + response.data)
-				// 		console.log("successful " + response.statusText)
-				// 		console.log("successful " + response)
-				// 	}, function(response) {
-				// 		console.log("Error " + response)
-				// 		console.log("Error " + response.data)
-				// 		console.dir(response)
-				// 	});
 
 				var request = $http({
 					method: "POST",
@@ -43,9 +27,6 @@ angular.module("myPortfolio")
 					console.log("successful " + response.data)
 					console.log("successful " + response.statusText)
 				}, function(response) {
-					for(var item in response) {
-						console.log(item)
-					}
 					console.log("error data: " + response.data)
 					console.log("error status: " + response.status)
 					console.log("error statusText: " + response.statusText)
--- app/templates/contact.php
+++ app/templates/contact.php
@@ -1,45 +1,42 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
+date_default_timezone_set('Etc/UTC');
 
-	function input_validation($formData) {
-		$formData = trim($formData);
-		$formData = stripslashes($formData);
-		$formData = htmlspecialchars($formData);
-		return $formData;
-	}
-	
-	$postdata = file_get_contents("php://input");
-    $request = json_decode($postdata);
-	$firstName = input_validation($request->firstName);
-	$lastName = input_validation($request->lastName);
-	$email = input_validation($request->email);
-	$phone = input_validation($request->phoneNumber);
-	$message = wordwrap(input_validation($request->message), 70, "\r\n");
-
-	$completed = wordwrap(
-		("Email from: " . $firstName . " " . $lastName . "\r\n"
-		. "phone: " . $phone . "\r\n" . "email: " . $email . "\r\n"
-		. "message: " . $message), 70, "\r\n");
-
-	$subject = "Mail from portfolio website.";
-
-$headers = array("From: from@example.com",
-    "Reply-To: replyto@example.com",
-    "X-Mailer: PHP/" . PHP_VERSION
-);
-$headers = implode("\r\n", $headers);
+// echo "File exists? " . file_exists('../../PHPMailer/PHPMailerAutoload.php');
+// require '../../PHPMailer/PHPMailerAutoload.php';
 
 
-	if( mail("krisb@tengundesign.com", $subject, $completed) ) {
-		echo "Mail successful!";
-		return;
-	} else {
-		echo "Mail failed to send";
-		return;
-	}
+
+function input_validation($formData) {
+	$formData = trim($formData);
+	$formData = stripslashes($formData);
+	$formData = htmlspecialchars($formData);
+	return $formData;
+}
+
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+$firstName = input_validation($request->firstName);
+$lastName = input_validation($request->lastName);
+$email = input_validation($request->email);
+$phone = input_validation($request->phoneNumber);
+$message = wordwrap(
+	"Email from: " . $firstName . " " . $lastName . "<" . $email . ">\r\n" 
+	. "Phone: " . $phone . "\r\nMessage:\r\n" . 
+	input_validation($request->message
+), 70, "\r\n");
+
+// $headers = "MIME-Version: 1.0" . "\r\n";
+// $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers = "From: <" . $email . ">" . "\r\n";
+$headers .= "Reply-to: " . $email . "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
 
+if(mail("krisb@tengundesign.com", "Message from Portfolio Website", $message)) {
+	echo "Successful sending";
+} else {
+	echo "Nope. Still not working.";
+}
+return;
 
 ?>
